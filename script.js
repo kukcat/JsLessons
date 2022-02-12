e15bf870afb589a4e43fa08a496407d2e3c580fc
kukcat <galaxycraft3103@gmail.com>
2022-02-12 18:12:13 +0200

    second lesson

--- script.js
+++ script.js
@@ -33,3 +33,81 @@ function changeFontSize(){
     text.style.fontSize = textSize + 'px'
 
 }
+
+function doubleNumber(){
+
+    let num = document.querySelector('.number_input').value
+    //console.log(num*2)
+    let output = document.querySelector('.output')
+    output.innerHTML = num*2
+
+}
+
+function forNumber(){
+
+    let num1 = document.querySelector('.number1_input').value
+    let num2 = document.querySelector('.number2_input').value
+
+
+    for(; +num1 <= +num2; num1++){
+        console.log(num1)
+    }
+
+}
+
+//============================================ lesson 2
+
+let isBlock = false
+let blockTop = 0;
+let size;
+
+function addBlock(){
+
+    if(isBlock == true) return
+
+    size = document.querySelector('.size_input').value
+    let wrapper = document.querySelector('.wrapper_block')
+
+    size = +size
+
+    if(size <= 50 || size>=500) return 
+
+    let block = document.createElement('div')
+    block.classList.add('block')
+    
+    block.style.width = size + 'px'
+    block.style.height = size + 'px'
+    
+    wrapper.insertAdjacentElement('afterbegin', block)
+    isBlock = true
+}
+
+function blockDown(){
+
+    let block = document.querySelector('.block')
+    
+    if(size + blockTop + 10 > 500) return
+    
+    blockTop += 10
+
+    block.style.marginTop = blockTop + 'px'
+}
+
+function blockUp(){
+
+    let block = document.querySelector('.block')
+    
+    if(blockTop <= 0) return
+    
+    blockTop -= 10
+
+    block.style.marginTop = blockTop + 'px'
+
+}
+
+
+// function addError(){
+//     let i = addBlock()
+//     let errorBlock = document.querySelector('.error')
+//     errorBlock.innerHTML = 'Вы указали неверные размеры:' + i
+// }
